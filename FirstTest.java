dcb2147bd27e28c93e41628e1d2b49bc60109c47
IrinaIalova <irina@dyna.mo>
2018-12-29 19:12:29 +0300

    Lesson2,Ex.2/FindWord
    Lesson2,Ex.2/FindWord

--- FirstTest.java
+++ FirstTest.java
@@ -3,7 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -37,9 +41,32 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest()
+    public void findWordTest()
     {
-        System.out.println("First test run");
+        WebElement element_to_init_search = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
+        element_to_init_search.click();
+
+        WebElement element_to_enter_search_line = waitForElementPresentByXpath(
+                "//*[contains(@text,'Search…')]",
+                "Test failed: Cannot find Search... input"
+        );
+    }
+
+
+    private WebElement waitForElementPresentByXpath(String xpath, String error_message, long timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "n");
+        By by = By.xpath(xpath);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String error_message)
+    {
+        return waitForElementPresentByXpath(xpath,error_message,5);
     }
 }
+
 
